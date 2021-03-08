571c5dfa6ea8f1e6b2464173c497847b76df5a74
Christine Su <christine.hui.su@gmail.com>
2021-03-08 18:39:26 -0500

    Add 3/8/21 code stretch subscribe.js

---
+++ unsubscribe.js
@@ -0,0 +1,39 @@
+// Code stretch 3/8/21
+
+const publisher = {
+  listeners: [],
+  history: [],
+  dispatch: function (message) {
+    this.history.push(message);
+    this.listeners.forEach((listener) => listener(message));
+  },
+  subscribe: function (listener) {
+    this.history.forEach((message) => listener(message));
+    this.listeners.push(listener);
+  },
+};
+
+const unsubscribe = function () {
+  publisher.listeners.shift();
+};
+
+publisher.subscribe((message) => {
+  console.log(`early listener: ${message}`);
+});
+publisher.dispatch("hey now!!");
+publisher.dispatch("hello newman");
+publisher.subscribe((message) => {
+  console.log(`late listener: ${message}`);
+});
+publisher.dispatch("HEEEEYY");
+unsubscribe();
+publisher.dispatch("HEEEEYY Again");
+/*
+  early listener: hey now!!
+  early listener: hello newman
+  late listener: hey now!!
+  late listener: hello newman
+  early listener: HEEEEYY
+  late listener: HEEEEYY
+  late listener: HEEEEYY Again
+  */
